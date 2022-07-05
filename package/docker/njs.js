bfea1d4988c6d0be068f765bfba5d7dfdd00bed8
Abinaya U <abinaya.u@thoughtworks.com>
2022-07-05 17:33:46 +0530

    refactor. variable declaration in patient-documents

--- package/docker/njs.js
+++ package/docker/njs.js
@@ -1,18 +1,18 @@
 function getRequestedDocumentPath(request) {
-	let decodedPath = decodeURI(request.args["requested_document"])
+	const decodedPath = decodeURI(request.args["requested_document"])
 	request.headersOut["x-requested-documents"] = decodedPath
 	return decodedPath
 }
 
 function isAuthorized(userPrivileges) {
-	var authorizedPrivileges = [
+	const authorizedPrivileges = [
 		"app:clinical",
 		"app:patient-documents",
 		"app:document-upload",
 	]
 
 	for (
-		var privilegeIndex = 0;
+		let privilegeIndex = 0;
 		privilegeIndex < userPrivileges.length;
 		privilegeIndex++
 	)
@@ -23,19 +23,19 @@ function isAuthorized(userPrivileges) {
 
 
 function auth(request) {
-	var documentPath = getRequestedDocumentPath(request)
+	const documentPath = getRequestedDocumentPath(request)
 	request.log(`Authenticating for Document request: ${documentPath}`)
 	request.subrequest(
 		`/openmrs/session/verify`,
 		{ method: "GET" },
 		function (res) {
 			if (res.status === 200) {
-				var jsonData = JSON.parse(res.responseBody)
+				const jsonData = JSON.parse(res.responseBody)
 				if (
 					jsonData.authenticated && jsonData.user.privileges &&
 					isAuthorized(jsonData.user.privileges)
 				) {
-					request.log(`User session ${jsonData.sessionId} is valid`)
+					request.log(`User session is valid`)
 					request.internalRedirect(
 						`/document/fetch?requested_document=${documentPath}`
 					)
