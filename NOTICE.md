9019a1520b8cc4a57536c4df40921bb775dfb263
ravinderkabli <ravinder.kabli@thoughtworks.com>
2026-08-27 10:40:31 +0530

    Add license transition files and apply headers (#24)
    Generated by Bahmni License Transition CLI.
    
    Changes included:
    - LICENSE: Full license text
    - FILE_HEADER.txt: Header template reference (if MPL v2)
    - HEADER_GUIDE.md: Header application guide (if MPL v2)
    - Source files: License headers applied automatically (if MPL v2)
    - pom.xml: Updated license information (if present)
    - package.json: Updated license information (if present)
    - NOTICE.md: Attribution file (if Thoughtworks contributions)
    
    JIRA: BAH-4645
    
    Co-authored-by: ravinderkabli <ravinderkabli@users.noreply.github.com>

---
+++ NOTICE.md
@@ -0,0 +1,9 @@
+# NOTICE
+
+## Patient Documents
+
+Copyright © 2026 
+
+### Contributors
+
+This repository has got source code generated at Thoughtworks.
--- package/docker/njs.js
+++ package/docker/njs.js
@@ -1,3 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://www.bahmni.org/license/mplv2hd.
+ *
+ * Copyright 2026. 
+ */
+
 function getRequestedDocumentPath(request) {
 	const decodedPath = decodeURI(request.args["requested_document"])
 	request.headersOut["x-requested-documents"] = decodedPath
